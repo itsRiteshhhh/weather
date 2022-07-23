dd1790295b5c6dfbc5373c395dbecf241b48cf03
Anonymous <anonymous@localhost.localdomain>
2022-07-23 22:40:23 +0530

    initial commit

---
+++ build/precache-manifest.68df40d67ad59548457bb2c21e1a34b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf06eaf7495313583ed4",
+    "url": "/static/js/main.bf06eaf7.chunk.js"
+  },
+  {
+    "revision": "e65162799c4670f3506c",
+    "url": "/static/js/1.e6516279.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0e2eddbed03cd52e3275d98029a11c6",
+    "url": "/static/media/icon.f0e2eddb.png"
+  },
+  {
+    "revision": "f3293538b2474135ccd3c1efa9857049",
+    "url": "/index.html"
+  }
+];
---
+++ src/ShowTemp.js
@@ -0,0 +1,94 @@
+import React from 'react'
+
+function ShowTemp({ text }) {
+    let temp = (text.temp - 273).toFixed(2);
+    let temp_minn = (text.temp_min - 273).toFixed(2);
+    let temp_maxx = (text.temp_max - 273).toFixed(2);
+
+    return (
+        <div class="container my-5 herodiv">
+            <div class="roww">
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Description</h5>
+                        <p class="card-text">{text.description}</p>
+                    </div>
+                </div>
+
+ 
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Temp </h5>
+                        <p class="card-text">{temp == -273 ? 0 : temp}°C</p>
+                    </div>
+                </div>
+
+
+                <div class="card">
+                    <div class="card-body ">
+                        <h5 class="card-title">Temp Min</h5>
+                        <p class="card-text">{temp_minn == -273 ? 0 : temp_minn}°C</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="roww">
+
+                <div class="card">
+
+                    <div class="card-body">
+                        <h5 class="card-title">Temp Max</h5>
+                        <p class="card-text">{temp_maxx == -273 ? 0 : temp_maxx}°C</p>
+                    </div>
+                </div>
+
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Humidity</h5>
+                        <p class="card-text">{text.humidity}%</p>
+                    </div>
+                </div>
+
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Sunrise</h5>
+                        <p class="card-text">{text.sunrise}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="roww" >
+
+                <div class="card ">
+                    <div class="card-body p-2">
+                        <h5 class="card-title">Sunset</h5>
+                        <p class="card-text">{text.sunset}</p>
+                    </div>
+                </div>
+
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Country</h5>
+                        <p class="card-text">{text.country}</p>
+                    </div>
+                </div>
+
+            </div >
+            <div>
+                <div class="homediv">
+                    <p class="text"> Have a GOOD DAY!!!</p>
+
+
+                </div>
+            </div>
+        </div >
+
+
+    )
+}
+
+export default ShowTemp;
