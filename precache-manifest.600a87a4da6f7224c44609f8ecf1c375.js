670050dc0130a00556c876e806eb5120cdbcb90f
Anonymous <anonymous@localhost.localdomain>
2022-07-24 18:14:41 +0530

    Updates

---
+++ precache-manifest.600a87a4da6f7224c44609f8ecf1c375.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa0e41744abb2754070d",
+    "url": "/weather/static/js/main.fa0e4174.chunk.js"
+  },
+  {
+    "revision": "e65162799c4670f3506c",
+    "url": "/weather/static/js/1.e6516279.chunk.js"
+  },
+  {
+    "revision": "177e1b728fb0d4ffdfea",
+    "url": "/weather/static/js/runtime~main.177e1b72.js"
+  },
+  {
+    "revision": "f0e2eddbed03cd52e3275d98029a11c6",
+    "url": "/weather/static/media/icon.f0e2eddb.png"
+  },
+  {
+    "revision": "fe85236ee29835319ab8e5a956191f0d",
+    "url": "/weather/index.html"
+  }
+];
